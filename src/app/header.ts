b8c45626f3bf771c19c88cc98d6676998bd9ba12
Andre <andrejales.com@gmail.com>
2025-10-29 10:52:48 -0300

    Create header and update styles

--- src/app/header.ts
+++ src/app/header.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component()
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
 export class Header {}
